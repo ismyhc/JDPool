318f17fb07a9da4993e4e1865f3d8583f2fcc3f0
Jacob Davis <ismyhc@gmail.com>
2017-02-25 13:40:34 -0500

    Initial commit

---
+++ JDPool.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IJDPoolable
+{
+    void Free();
+}
+
+public class JDPool<T>
+{
+    
+	readonly List<T> _items = new List<T>();
+    readonly Queue<T> _freeItems = new Queue<T>();
+	readonly Func<T> _createItemAction;
+
+	public JDPool(Func<T> createItemAction)
+	{
+		_createItemAction = createItemAction;
+	}
+
+	public sealed void FreeItem(T item)
+	{
+        
+        var poolableItem = item as IJDPoolable;
+
+        if (poolableItem != null)
+            poolableItem.Free();
+        
+		_freeItems.Enqueue(item);
+
+	}
+
+    public sealed void Clear()
+    {
+        _items.Clear();
+        _freeItems.Clear();
+    }
+
+	public sealed T GetItem()
+	{
+		if (_freeItems.Count == 0)
+		{
+			T item = _createItemAction();
+			_items.Add(item);
+
+			return item;
+		}
+
+		return _freeItems.Dequeue();
+	}
+
+    public sealed int FreeItemCount()
+	{
+		return _freeItems.Count;
+	}
+
+}
